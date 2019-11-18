261b2ac79ec745b4851f072cc9582d828562e3fd
kealanheena <kealanheena@gmail.com>
2019-11-18 15:38:59 +0000

    add initilze tests for setting balance and edit code to pass

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,16 @@
-describe Oystercard do
+require "oystercard"
 
+describe Oystercard do
+  describe "#initilize" do
+
+    it "has a balance of '0' by default" do
+      expect(subject.balance).to eq 0
+    end
+
+    it "returns balance given when user specifies balance" do
+      user_input = rand(5)
+      card = Oystercard.new(user_input)
+      expect(card.balance).to eq user_input
+    end
+  end
 end
